f9b2679728680029ff46f1ebc3f0f3d20033a86e
SheetalSalam <106577570+SheetalSalam@users.noreply.github.com>
2022-06-06 14:37:35 +0530

    Update test.java
    adding sysout line

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 Class test{
 	public static void main(String args[]){
 		System.out.println("this is inside git");
+		System.out.println("I can edit using the pencil icon. (edit this file option)");
+		
 	}
 }
